db613c5a928324f05b51176ba6eb8736efafef13
Lance Pollard <lancejpollard@gmail.com>
2010-08-06 01:36:22 -0600

    first commit

---
+++ app.rb
@@ -0,0 +1,97 @@
+require 'rubygems'
+gem "rack", '~> 1.2.1'
+require 'sinatra'
+require 'action_view'
+require 'sinatra/content_for'
+require 'sinatra/ratpack'
+require 'insightful'
+require 'broadway'
+require 'haml'
+require 'json'
+require 'open-uri'
+require 'ubiquitously'
+
+helpers do
+  include Insightful
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::SanitizeHelper
+  extend ActionView::Helpers::SanitizeHelper::ClassMethods
+  def nested_path(post)
+    post.path
+  end
+end
+
+this = File.expand_path(File.dirname(__FILE__))
+SITE = Broadway.build(:source => "#{this}/_content", :settings => "#{this}/_config.yml")
+
+enable :sessions
+set :public, this
+set :views, "views"
+
+def urlify(url)
+  url = "http://#{url}" if url !~ /^http/
+  url
+end
+
+def create(params)
+  puts params.inspect
+  user = Ubiquitously::User.new(
+    :username => "viatropos",
+    :cookies_path => "_cookies.yml"
+  )
+  tags = params["tags"].split(" ") if params["tags"]
+  post = Ubiquitously::Post.new(
+    :url => urlify(params["url"]),
+    :title => params["title"],
+    :description => params["description"],
+    :tags => tags,
+    :user => user
+  )
+  puts post.inspect
+  #post.save(params["services"])
+end
+
+get "/" do
+  haml :index
+end
+
+post "/" do
+  create(params)
+end
+
+post "/start" do
+  begin
+    url = urlify(params["url"])
+    html = Nokogiri::HTML(open(url).read)
+    title = html.xpath("//title").first.text
+    description = html.xpath("//meta[@name='description']").first["content"] rescue ""
+    tags = html.xpath("//meta[@name='keywords']").first["content"] rescue ""
+    tags = tags.split(",").map do |tag|
+      tag.strip.downcase.gsub(/[^a-z0-9]/, "-").squeeze("-")
+    end.join(", ")
+    
+    post = {
+      :url         => url,
+      :title       => title,
+      :description => description,
+      :tags        => tags
+    }
+    JSON.generate(post)
+  rescue Exception => e
+    puts e.backtrace
+    "error"
+  end
+end
+
+post "/finish" do
+  create(params)
+end
+
+get "/*" do
+  ""
+end
+
+not_found do
+  ""
+end
---
+++ javascripts/application.js
@@ -0,0 +1,143 @@
+// http://maettig.com/code/css/text-shadow.html
+$(document).ready(function() {
+  var form = $("#content-form");
+  var stepOne = $("#step-one");
+  var stepTwo = $("#step-two");
+  var stepThree = $("#step-three");
+  
+  function setupSubmit(submit, position) {
+    submit.addClass("next-step").addClass("step-one").attr("data-step", "1");
+    submit.css("top", stepOne.height()/2);
+    submit.css(position, - submit.width() - 20);
+  }
+  
+  var submit = $("#next");
+  setupSubmit(submit, "right");
+  setupSubmit($("#previous"), "left");
+  
+  stepTwo.height(stepOne.height());
+  
+  var services = [];
+  
+  form.ajaxForm({
+    dataType: 'json',
+    beforeSubmit: function(formData, jqForm, options) {
+      var firstStep = true;
+      var props = ["title", "tags", "description", "rating"]
+      for (var prop in formData) {
+        if (props.indexOf(formData[prop].name) != -1 && formData[prop].value) {
+          firstStep = false;
+        }
+        if (formData[prop].name == "services[]") {
+          services.push(formData[prop].value);
+        }
+      }
+      services = $.unique(services);
+      
+      if (firstStep) {
+        options.url = "/start";
+        $.jGrowl("Loading metadata for " + $("#url-input").val(), {
+          life: 1500,
+          beforeOpen: function(e, m, o) {
+            
+          }
+        });
+      } else {
+        if (form.attr("data-step") == "step-two") {
+          var service = services.pop();
+          if (!service) {
+            return false;
+          }
+          
+          $("input.service[value=" + service + "]").attr("checked", false).attr("disabled", true);
+          options.url = "/finish";
+          $.jGrowl("Publishing to " + service + "...", {
+            life: 1500,
+            beforeOpen: function(e, m, o) {
+              
+            },
+            beforeClose: function() {
+              setTimeout(function() {
+                form.trigger("submit");
+              }, 300);
+              $.jGrowl("<a href='http://digg.com/'>http://digg.com/</a>", {
+                header: "Digg Link...",
+                sticky: true
+              });
+            }
+          });
+        } else {
+          return false;
+        }
+      }
+    },
+    success: function(post, statusText, xhr, $form) {
+      for (var property in post) {
+        var input = $("#" + property + "-input");
+        if (!input.val())
+          input.val(post[property]);
+      }
+    }
+  });
+  
+  stepTwo.css("display", "none");
+  stepThree.css("display", "none");
+  
+  $("#url-input").bind("keydown", "tab", function() {
+    form.trigger("submit");
+  });
+  $("#tags-input").bind("keydown", "tab", function() {
+    $("#next").trigger("click");
+  });
+  $("#submit").bind("keydown", "tab", function() {
+    $("#previous").trigger("click");
+  });
+  
+  $(window).bind("keydown", "left", function() {
+    var target = $(event.target);
+    $("#previous").trigger("click");
+  });
+  $(window).bind("keydown", "right", function() {
+    var target = $(event.target);
+    $("#next").trigger("click");
+  });
+  
+  var pagers = $(".pager");
+  
+  function updateStep(step) {
+    var current_step = form.attr("data-step");
+    if (pagers.hasClass(current_step))
+      pagers.removeClass(current_step);
+    pagers.addClass(step);
+    form.attr("data-step", step);
+  }
+  
+  updateStep("step-one");
+  
+  var state = false;
+  $('#content-form .steps').cycle({
+			fx:	"scrollHorz", // You can choose any effects that you like, from the Link reference : http://www.malsup.com/jquery/cycle/browser.html
+			timeout: 0,
+			next: "#next",
+			prev: "#previous",
+			manualTrump: false,
+			sync: true,
+			speed: 0,
+			nowrap: true,
+			prevNextClick: function(isNext, index, slideElement) {
+			  var pagers = $(".pager");
+			  if (index == 0) {
+			    updateStep("step-one");
+			    $("#previous").css("display", "none");
+			    $("#next").css("display", "block");
+			  } else if (index == 1) {
+			    updateStep("step-two");
+			    $("#previous").css("display", "block");
+			    $("#next").css("display", "none");
+			  }
+			}
+	}, true);
+	$("a.login").click(function() {
+	  return false;
+	});
+});
